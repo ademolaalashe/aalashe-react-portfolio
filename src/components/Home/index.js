e04627591bf8a9b82fc4e97fadca86d148b42f72
ademolaalashe <77229886+ademolaalashe@users.noreply.github.com>
2023-03-17 01:59:50 +0000

    Add first code

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,23 @@
+import {Link} from 'react-router-dom';
+// import LogoTitle from '../../assets/images/(add file here and uncomment)'
+import './index.scss'
+
 const Home = () => {
 
 
     return (
         <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi, <br /> I'm 
+                <img src={LogoTitle} alt="developer" />
+                Ademola Alashe
+                <br />
+                a Web Developer
+                </h1>
+                <h2>Frontend Developer</h2>
+                <Link to= "/contact" className='flat-button'>CONTACT ME</Link>
 
+            </div>
 
         </div>
     )
